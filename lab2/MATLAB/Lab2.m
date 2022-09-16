540fb88b47190943f37b77bf965cac8ef2fcde98
David <jdiazga@unal.edu.co>
2022-09-16 13:29:36 -0500

    Add MATLAB Script

---
+++ lab2/MATLAB/Lab2.m
@@ -0,0 +1,24 @@
+%
+rosinit; %Conexión con nodo maestro
+%%
+velPub = rospublisher("/turtle1/cmd_vel","geometry_msgs/Twist"); %Creación publicador
+velMsg = rosmessage(velPub); %Creación de mensaje
+%%
+velMsg.Linear.X = 0;
+velMsg.Linear.Y = 1; %Valor del mensaje
+send(velPub,velMsg); %Envio
+pause(1)
+%% Suscripción al topic pose y obtención de posición actual
+SUB = rossubscriber("/turtle1/pose");
+message = SUB.LatestMessage
+%% Uso del servicio de teleport_absolute
+SVCINF = rosservice("info","/turtle1/teleport_absolute")
+[CLIENT,REQUEST] = rossvcclient("/turtle1/teleport_absolute","DataFormat","struct")
+REQUEST.X = single(6);
+REQUEST.Y = single(8);
+theta = deg2rad(45);
+REQUEST.Theta = single(theta);
+waitForServer(CLIENT,"Timeout",3)
+response = call(CLIENT,REQUEST)
+%% Desconexión del nodo maestro
+rosshutdown
