da4584d8a8fe3aa69cd0923b137be96cbbe8f4a8
David <jdiazga@unal.edu.co>
2022-09-18 19:52:45 -0500

    Change Lab2.m

--- lab2/MATLAB/Lab2.m
+++ lab2/MATLAB/Lab2.m
@@ -1,11 +1,9 @@
-%
 rosinit; %Conexión con nodo maestro
 %%
 velPub = rospublisher("/turtle1/cmd_vel","geometry_msgs/Twist"); %Creación publicador
 velMsg = rosmessage(velPub); %Creación de mensaje
 %%
-velMsg.Linear.X = 0;
-velMsg.Linear.Y = 1; %Valor del mensaje
+velMsg.Linear.X = 1; %Valor del mensaje
 send(velPub,velMsg); %Envio
 pause(1)
 %% Suscripción al topic pose y obtención de posición actual
@@ -14,8 +12,8 @@ message = SUB.LatestMessage
 %% Uso del servicio de teleport_absolute
 SVCINF = rosservice("info","/turtle1/teleport_absolute")
 [CLIENT,REQUEST] = rossvcclient("/turtle1/teleport_absolute","DataFormat","struct")
-REQUEST.X = single(6);
-REQUEST.Y = single(8);
+REQUEST.X = single(2);
+REQUEST.Y = single(3);
 theta = deg2rad(45);
 REQUEST.Theta = single(theta);
 waitForServer(CLIENT,"Timeout",3)
