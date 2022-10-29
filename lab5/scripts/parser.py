12612db9a245149b5255bb7aa35a90a6e6f54a37
jhairssteven <71862429+jhairssteven@users.noreply.github.com>
2022-10-29 17:15:07 -0500

    lab5: Add trajectories text files

---
+++ lab5/scripts/parser.py
@@ -0,0 +1,21 @@
+# Script to parse text files with data points to incorporate into the Phantom X movement
+
+from fileinput import filename
+
+def getTrajectoryFromTextFile(filename):
+    with open(filename, 'r') as fobj:
+        trajectory = []
+        for line in fobj:
+            coords_str = line.split()[-1][1:-2].split(",")
+            coords = [float(n) for n in coords_str]
+            trajectory.append(coords)
+
+    return trajectory
+
+
+file_names = ["circle.txt", "custom_part.txt", "d_letter.txt",
+                "inner_ring.txt", "line123.txt", "outter_ring.txt",
+                "point1to5.txt", "s_letter.txt", "triangle.txt"]
+
+# trajectory = getTrajectoryFromTextFile(file_names[0])
+# print(trajectory)
