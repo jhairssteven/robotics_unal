da166b53ed80d7e1536b5d1c946bb272f0c50d22
David <jdiazga@unal.edu.co>
2022-10-29 17:05:54 -0500

    Lab 5: Add scripts to control PhantomX

---
+++ lab5/scripts/jointCommand.py
@@ -0,0 +1,14 @@
+import rospy
+import time
+from dynamixel_workbench_msgs.srv import DynamixelCommand
+
+def jointCommand(command, id_num, addr_name, value, time):
+    rospy.wait_for_service('dynamixel_workbench/dynamixel_command')
+    try:        
+        dynamixel_command = rospy.ServiceProxy(
+            '/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+        result = dynamixel_command(command,id_num,addr_name,value)
+        rospy.sleep(time)
+        return result.comm_result
+    except rospy.ServiceException as exc:
+        print(str(exc))
